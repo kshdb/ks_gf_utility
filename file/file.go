6c7f226b8a72791d1e445115808c24bf008a5152
sh <dfsxh@163.com>
2025-06-26 16:37:28 +0800

    多文件上传封装

--- file/file.go
+++ file/file.go
@@ -7,18 +7,22 @@ import (
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gfile"
 	"github.com/gogf/gf/v2/os/gtime"
+	"io"
+	"mime/multipart"
+	"os"
 )
 
 /*
 文件对象
 */
 type FileInfo struct {
-	Ctx        g.Ctx             //上下文
-	File       *ghttp.UploadFile `json:"avatarfile" type:"file" dc:"选择上传文件"`
-	Name       string            `json:"name"  dc:"自定义文件名称"`        // 自定义文件名称
-	PathR      string            `json:"pathR"  dc:"自定义二级文件目录"`     // 自定义二级文件目录
-	RandomName bool              `json:"randomName"  dc:"是否随机命名文件"` // 是否随机命名文件
-	FileType   string            `json:"fileType"  dc:"文件类型"`       // 文件类型
+	Ctx        g.Ctx //上下文
+	File       *ghttp.UploadFile
+	Files      []*multipart.FileHeader
+	Name       string `json:"name"  dc:"自定义文件名称"`        // 自定义文件名称
+	PathR      string `json:"pathR"  dc:"自定义二级文件目录"`     // 自定义二级文件目录
+	RandomName bool   `json:"randomName"  dc:"是否随机命名文件"` // 是否随机命名文件
+	FileType   string `json:"fileType"  dc:"文件类型"`       // 文件类型
 }
 
 /*
@@ -32,6 +36,32 @@ func (f *FileInfo) Upload() (fileName string, err error) {
 		dateDirName := gtime.Now().Format("Ymd")
 		fileName, err = f.File.Save(gfile.Join(uploadPath, f.PathR, dateDirName), f.RandomName)
 		fileName = fmt.Sprintf("/%s/%s/%s/%s", uploadPath, f.PathR, dateDirName, fileName)
+	}
+	return
+}
+
+/*
+多文件上传
+*/
+func (f *FileInfo) Uploads(_files []*multipart.FileHeader) (fileNames []string, err error) {
+	uploadPath := g.Cfg().MustGet(f.Ctx, "ks_gf_file.file.upload.path").String()
+	if len(_files) > 0 {
+		for _, _f := range _files {
+			//打开上传文件
+			file, _ := _f.Open()
+			defer file.Close()
+			//创建上传目录
+			if !gfile.Exists(uploadPath) {
+				os.Mkdir(uploadPath, os.ModePerm)
+			}
+			dateDirName := gtime.Now().Format("Ymd")
+			//创建上传文件
+			fileName := fmt.Sprintf("%s/%s/%s/%s", uploadPath, f.PathR, dateDirName, _f.Filename)
+			cur, _ := os.Create(fileName)
+			defer cur.Close()
+			io.Copy(cur, file)
+			fileNames = append(fileNames, _f.Filename)
+		}
 	}
 	return
 }
